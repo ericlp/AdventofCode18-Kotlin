a92e754291d4b5fe321f9fee710fe287a9795435
Eric Carlsson <caeric@student.chalmers.se>
2018-12-03 15:55:30 +0100

    Dayay2

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,32 +1,23 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val lines = File("assets/day2.txt").readText().split("\n")
-    part1(lines)
+    val lines = File("assets/day2.txt").readText().split('\n')
+    part2(lines)
 }
 
 private fun part1 (lines: List<String>) {
-
-    var has2 = false
-    var has3 = false
 
     var check2 = 0
     var check3 = 0
 
     var map: MutableMap<Char,Int> = HashMap()
 
-    var freq: Int?
-
     for (str in lines) {
         for (chr in str) {
-            freq = map.put(chr, map.getOrDefault(chr, 0) + 1)
-            when (freq) {
-                1 -> if (!has2) {has2 = true; check2++}
-                2 -> if (!has3) {has3 = true; check3++}
-            }
+            map.put(chr, map.getOrDefault(chr, 0) + 1)
         }
-        has2 = false
-        has3 = false
+        if (map.containsValue(2)) { check2++ }
+        if (map.containsValue(3)) { check3++ }
         map.clear()
     }
 
@@ -35,4 +26,27 @@ private fun part1 (lines: List<String>) {
 }
 
 private fun part2 (lines: List<String>) {
+
+    println(lines.map { compareAll(it, lines) }.filter { it != null })
+}
+
+private fun compareAll (str: String, lines: List<String>) :String?{
+
+    var balans: Int = 0
+    var diffIndex: Int = 0
+
+    for (line in lines) {
+        if (line == str) continue
+        for (ch in line.indices) {
+            if (line[ch] !== str[ch]) {
+                balans++
+                diffIndex = ch
+            }
+            if (balans > 1) continue
+        }
+        if (balans == 1) return str.substring(0,diffIndex) + str.substring(diffIndex+1,str.length)
+        balans = 0
+    }
+    return null
+
 }
